8553a2d70a116a4bbc191caadd0649ba9df37543
Pratham1730 <prathampatel1730@gmail.com>
2025-03-04 16:30:16 +0530

    arrow change

---
+++ app/src/main/java/com/example/hrms/CustomSpinnerAdapter.kt
@@ -0,0 +1,25 @@
+import android.content.Context
+import android.graphics.Color
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+// Custom adapter to ensure spinner text remains black
+class CustomSpinnerAdapter(context: Context, items: Array<String>) :
+    ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, items) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = super.getView(position, convertView, parent) as TextView
+        view.setTextColor(Color.BLACK) // Set selected item text color to black
+        return view
+    }
+
+//    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+//        val view = super.getDropDownView(position, convertView, parent) as TextView
+//        view.setTextColor(Color.BLACK) // Set dropdown item text color to black
+//        return view
+//    }
+
+
+}
--- app/src/main/java/com/example/hrms/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/hrms/activity/SignUpActivity.kt
@@ -1,9 +1,17 @@
 package com.example.hrms.activity
 
+import CustomSpinnerAdapter
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.util.Patterns
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,60 +21,100 @@ import com.example.hrms.R
 import com.example.hrms.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private var department: String = "Department"
+    private var position: String = "Position"
+    private var gender: String = "Gender"
     private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         listeners()
-
-        // test commit
-
+        genderSpinner()
+        positionSpinner()
+        departmentSpinner()
     }
 
-    private fun listeners(){
+    private fun listeners() {
         binding.btnSignUp.setOnClickListener {
             validations()
         }
-        binding.txtMoveToSignInPage.setOnClickListener{
-            startActivity(Intent(this@SignUpActivity , SignInActivity::class.java))
+        binding.txtMoveToSignInPage.setOnClickListener {
+            startActivity(Intent(this@SignUpActivity, SignInActivity::class.java))
             finish()
         }
     }
 
-    private fun validations(){
-        if (binding.edtSignUpName.text.toString().isEmpty()){
+    private fun validations() {
+        if (binding.edtSignUpName.text.toString().isEmpty()) {
             binding.edtSignUpName.error = "Name Not Entered"
             binding.edtSignUpName.requestFocus()
-        }
-        else if (binding.edtSignUpEmail.text.toString().isEmpty()){
-            binding.edtSignUpEmail.error = "Last Name Not Entered"
+        } else if (binding.edtSignUpEmail.text.toString().isEmpty()) {
+            binding.edtSignUpEmail.error = "Email Not Entered"
             binding.edtSignUpEmail.requestFocus()
-        }
-        else if (!Patterns.EMAIL_ADDRESS.matcher(binding.edtSignUpEmail.text.toString()).matches()){
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(binding.edtSignUpEmail.text.toString()).matches()) {
             binding.edtSignUpEmail.error = "Please Enter Valid Email"
             binding.edtSignUpEmail.requestFocus()
-        }
-//        else if (binding.edtSignUpDOB.text.toString().isEmpty()){
-//            binding.edtSignUpDOB.error = "DOB Not Entered"
-//            binding.edtSignUpDOB.requestFocus()
-//        }
-        else if (binding.edtSignUpPassword.text.toString().isEmpty()){
+        } else if (binding.edtSignUpPassword.text.toString().isEmpty()) {
             binding.edtSignUpPassword.error = "Password Not Entered"
             binding.edtSignUpPassword.requestFocus()
-        }
-        else if (binding.edtSignUpConfirmPassword.text.toString().isEmpty()){
-            binding.edtSignUpConfirmPassword.error = "Password Not Entered"
+        } else if (binding.edtSignUpConfirmPassword.text.toString().isEmpty()) {
+            binding.edtSignUpConfirmPassword.error = "Confirm Password Not Entered"
             binding.edtSignUpConfirmPassword.requestFocus()
-        }
-        else if(binding.edtSignUpPhoneNumber.text.toString().isEmpty()){
+        } else if (binding.edtSignUpPhoneNumber.text.toString().isEmpty()) {
             binding.edtSignUpPhoneNumber.error = "Phone Number Not Entered"
             binding.edtSignUpPhoneNumber.requestFocus()
-        }
-        else{
+        } else {
             Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show()
         }
     }
 
+    private fun genderSpinner() {
+        val genderList = arrayOf("Gender", "Male", "Female")
+        val adapter = CustomSpinnerAdapter(this, genderList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.genderSpinner.adapter = adapter
+
+        binding.genderSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                gender = genderList[p2]
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
+    }
+
+    private fun positionSpinner() {
+        val positionList = arrayOf("Position", "Intern", "Junior", "Senior")
+        val adapter = CustomSpinnerAdapter(this, positionList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.positionSpinner.adapter = adapter
+
+        binding.positionSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                position = positionList[p2]
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
+    }
+
+    private fun departmentSpinner() {
+        val departmentList = arrayOf("Department", "Android", "PHP", "iOS")
+        val adapter = CustomSpinnerAdapter(this, departmentList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.departmentSpinner.adapter = adapter
+
+        binding.departmentSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                department = departmentList[p2]
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
+    }
 }
+

ff8bae5eb3547e0c5421be8e9cfa0194471a3ca3
Pratham1730 <prathampatel1730@gmail.com>
2025-03-04 13:04:31 +0530

    04-03

---
+++ app/src/main/java/com/example/hrms/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.hrms.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hrms.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}

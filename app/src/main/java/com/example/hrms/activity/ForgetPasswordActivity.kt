6a09587bb9427334195a0389ffa0daa68adc3885
Ayushi Nagar <149509338+ayushinagar2004@users.noreply.github.com>
2025-03-04 15:07:44 +0530

    UPDATE

--- app/src/main/java/com/example/hrms/activity/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/hrms/activity/ForgetPasswordActivity.kt
@@ -2,11 +2,7 @@ package com.example.hrms.activity
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.hrms.R
 import com.example.hrms.databinding.ActivityForgetPasswordBinding
 
 class ForgetPasswordActivity : AppCompatActivity() {

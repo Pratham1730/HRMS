de2859717799af1d49ea9ea5669d4d81a57f2c32
Pratham1730 <prathampatel1730@gmail.com>
2025-03-05 17:41:04 +0530

    arrow change

--- app/src/main/java/com/example/hrms/Models/UserModel.kt
+++ app/src/main/java/com/example/hrms/Models/UserModel.kt
@@ -1,14 +1,19 @@
 package com.example.hrms.Models
 
+import java.math.BigInteger
+import java.sql.Date
+
 data class UserModel(
+    val insert : String,
     val name: String,
     val email: String,
     val password: String,
-    val phone: String,
-    val dob: String,
+    val phone: BigInteger,
+    val gender: Int,
+    val department: Int,
+    val position: Int,
+    val salary : Int,
     val joiningDate: String,
-    val gender: String,
-    val position: String,
-    val department: String
+    val dob: String
 )
 

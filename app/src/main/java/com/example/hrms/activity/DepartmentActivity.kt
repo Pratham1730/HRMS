7042d6974f3f5c0a22b1586458b5f99b20a7cea4
Ayushi Nagar <149509338+ayushinagar2004@users.noreply.github.com>
2025-03-11 09:49:51 +0530

    department

--- app/src/main/java/com/example/hrms/activity/DepartmentActivity.kt
+++ app/src/main/java/com/example/hrms/activity/DepartmentActivity.kt
@@ -1,17 +1,24 @@
 package com.example.hrms.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.hrms.R
+import com.example.hrms.databinding.ActivityDepartmentBinding
 
 class DepartmentActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDepartmentBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityDepartmentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(R.layout.activity_department)
+        binding.btnDepartmentBack.setOnClickListener {
+            startActivity(Intent(this,HomeActivity::class.java))
+        }
 
     }
 }

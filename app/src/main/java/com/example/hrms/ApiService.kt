c95ba3d18c05e7571ad3fd2ace721b53944e0b30
Pratham1730 <prathampatel1730@gmail.com>
2025-03-12 13:10:56 +0530

    update apiservice

--- app/src/main/java/com/example/hrms/ApiService.kt
+++ app/src/main/java/com/example/hrms/ApiService.kt
@@ -11,6 +11,7 @@ import com.example.hrms.responses.LeaveRequestResponse
 import com.example.hrms.responses.LeaveTypeResponse
 import com.example.hrms.responses.LoginResponse
 import com.example.hrms.responses.PositionResponse
+import com.example.hrms.responses.PublicHolidaysResponse
 import com.example.hrms.responses.UpdateDataResponse
 import com.example.hrms.responses.UpdatePasswordResponse
 import com.example.hrms.responses.UserDataResponse
@@ -167,5 +168,6 @@ interface ApiService {
         @Field("method") method : String,
         @Field("company_id") company_id : Int
     ) : Observable<PublicHolidaysResponse>
+
 
 }
